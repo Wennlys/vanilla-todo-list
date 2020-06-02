b326a697d80d2e3fc8329908cdcb7616ace0bf38
wennlys <wennlys@gmail.com>
2020-06-02 04:41:41 -0300

    couldn't delete new rendered lines bug fix

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
 'use strict';
 
 window.addEventListener('load', () => App.render());
-window.addEventListener('hashchange', () => App.render());;
+window.addEventListener('hashchange', () => App.render());
 
-//********************************
+//* *******************************
 
 var Home = (function Home() {
   var todosCollection = JSON.parse(localStorage.getItem('todoStorage')) || [];
 
-  return { render, after_render };
+  return { render, afterRender };
 
-  //********************************
+  //* *******************************
 
-  function renderTodo(todo) {
+  async function renderTodo(todo) {
     const todoHtml = `
       <li class="todo-item" data-key="${todo.id}">
         <label for="${todo.id}" class="isRead">
@@ -21,9 +21,15 @@ var Home = (function Home() {
         <span>${todo.description}</span>
         <button class="delete" value="${todo.id}"> X </button>
       </li>
-    `
+    `;
 
-    document.querySelector('.list').insertAdjacentHTML('beforeend', todoHtml);
+    await document.querySelector('.list').insertAdjacentHTML('beforeend', todoHtml);
+
+    document
+      .querySelector(`[value="${todo.id}"]`)
+      .addEventListener('click', function removeOnClick() {
+        deleteTodo(todo.id);
+      });
   }
 
   function saveTodo(todo) {
@@ -35,8 +41,8 @@ var Home = (function Home() {
     var todo = {
       description: todoDescription,
       done: false,
-      id: Date.now()
-    }
+      id: Date.now(),
+    };
 
     saveTodo(todo);
     renderTodo(todo);
@@ -45,17 +51,18 @@ var Home = (function Home() {
   function deleteTodo(id) {
     todosCollection = todosCollection.filter(function findByIdAndRemove(todo) {
       return !(todo.id == id);
-    })
+    });
     localStorage.setItem('todoStorage', JSON.stringify(todosCollection));
+
     const elementToBeDeleted = document.querySelector(`[data-key='${id}']`);
-    elementToBeDeleted.parentNode.removeChild(elementToBeDeleted);
+    elementToBeDeleted.remove();
   }
 
   function loadTodos() {
     if (todosCollection) {
       todosCollection.forEach(function renderEach(todo) {
         renderTodo(todo);
-      })
+      });
     }
   }
 
@@ -65,7 +72,7 @@ var Home = (function Home() {
        <h1> Home </h1>
        <ul class="list"></ul>
        <label for="todoDescription" class="description">
-         <input id="todoDescription" />
+         <input id="todoDescription"/>
        </label>
        <button class="add"> Add ToDo </button>
        <button class="to"> About </button>
@@ -73,47 +80,40 @@ var Home = (function Home() {
    `;
   }
 
-  function after_render() {
+  function afterRender() {
     loadTodos();
-
-    var descriptionInput = document.querySelector('#todoDescription');
 
     document
       .querySelector('.add')
       .addEventListener('click', function addOnClick() {
         createTodo();
-      })
-      descriptionInput.addEventListener('keyup', function addOnKeyup(event) {
+      });
+
+    document
+      .querySelector('#todoDescription')
+      .addEventListener('keyup', function addOnKeyup(event) {
         if (event.key == 'Enter') {
           createTodo();
         }
       });
 
-    document.querySelectorAll('.delete').forEach(function addDeleteFunctionToButton(deleteButton) {
-        deleteButton.addEventListener('click', function removeOnClick() {
-          deleteTodo(deleteButton.value);
-        });
-      })
-
     document
       .querySelector('.to')
-      .addEventListener('click', function redirectTo() { location.hash = '/about' });
-
-    //********************************
+      .addEventListener('click', function redirectTo() { location.hash = '/about'; });
 
     function createTodo() {
-      const todoDescription = descriptionInput.value;
+      let todoDescription = document.querySelector('#todoDescription').value;
       if (todoDescription.trim() != '') {
         addTodo(todoDescription);
       }
     }
   }
-})()
+}());
 
 var About = (function About() {
-  return { render, after_render };
+  return { render, afterRender };
 
-  //********************************
+  //* *******************************
 
   function render() {
     return `
@@ -124,12 +124,12 @@ var About = (function About() {
     `;
   }
 
-  function after_render() {
-    document.getElementById('btn').addEventListener('click', function onClick() {
-      location.hash = '/';
-    });
+  function afterRender() {
+    document
+      .getElementById('btn')
+      .addEventListener('click', function onClick() { location.hash = '/'; });
   }
-})()
+}());
 
 var App = (function App() {
   var root = document.getElementById('root');
@@ -141,17 +141,17 @@ var App = (function App() {
 
   return { render };
 
-  //********************************
+  //* *******************************
 
   async function render() {
     const url = requestURL();
     const page = routes[url] || Home;
     root.innerHTML = await page.render();
-    await page.after_render();
+    await page.afterRender();
   }
 
   function requestURL() {
     const [, resource = ''] = location.hash.slice(1).toLowerCase().split('/');
-    return ('/' + resource);
+    return (`/${resource}`);
   }
-})();
+}());
